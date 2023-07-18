f5afc7d27617bfc7ac05cac34ff6be4c4ae4eba4
Abdulvahobjon <abdulvahob0714@gmail.com>
2023-07-19 00:01:03 +0500

    commid

--- public/app.js
+++ public/app.js
@@ -41,3 +41,22 @@ const swiperOffers = new Swiper('.swiper-offers', {
     },
   }
 });
+
+document.querySelectorAll('.dropdown').forEach((drop) => {
+  if (drop) {
+    drop.addEventListener('click', () => {
+      drop.lastElementChild.classList.toggle('hidden')
+      drop.firstElementChild.classList.toggle('rotate-180')
+    })
+
+  }
+})
+
+document.querySelectorAll('.options').forEach((option) => {
+  if (option) {
+    option.addEventListener('click', (e) => {
+      e.target.parentElement.previousElementSibling.value = e.target.textContent
+      // console.log(e.target.textContent);
+    })
+  }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
       },
       backgroundImage: {
         'card': "transparent linear-gradient(180deg, #00000000 0%, #00000080 100%) 0% 0% no-repeat padding-box;"
+      },
+      fontSize:{
+        '46':["46px" , "57px"]
       }
     },
   },
